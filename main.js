7c000359ef15d82fe0fb62de2cfcbccdfcadb591
scrof90 <asknyshev@gmail.com>
2021-10-09 02:17:07 +0300

    iterate modules

--- main.js
+++ main.js
@@ -3,14 +3,46 @@ const O = 'o';
 
 const gameBoard = (() => {
   let board = [X, X, X, null, null, O, O, O, X];
+  const getBoard = () => board;
+  const markCell = (mark, index) => {
+    board[index] = mark;
+  };
+  return {
+    getBoard,
+    markCell,
+  };
 })();
 
-const displayController = (() => {})();
+const displayController = (() => {
+  const cells = Array.from(document.querySelectorAll('.js-cell'));
+  const updateBoard = () => {
+    const board = gameBoard.getBoard();
+    board.forEach((el, i) => {
+      if (!el) {
+        cells[i].classList.remove('tic', 'tac');
+      } else {
+        const mark = el === X ? 'tic' : 'tac';
+        cells[i].classList.add(mark);
+      }
+    });
+  };
+  return {
+    updateBoard,
+  };
+})();
 
-const Player = (mark) => {
-  const _mark = mark;
+const Player = (play) => {
+  const mark = play;
   return {};
 };
 
-const playerOne = Player(X);
-const playerTwo = Player(O);
+const game = (() => {
+  let playerOne = Player(X);
+  let playerTwo = Player(O);
+  let currentPlayer = playerOne;
+  return {};
+})();
+
+(() => {
+  displayController.updateBoard();
+})();
